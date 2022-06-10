76623f2a88e73c57aa0363fa51dac5c4e34369c1
vodinhtuan <vodinhtuan174@gmail.com>
2022-06-10 21:15:20 +0700

    update

---
+++ src/ss1_Introduction_to_Java/Thuchanh/System_Time.java
@@ -0,0 +1,11 @@
+package ss1_Introduction_to_Java.Thuchanh;
+
+import java.util.Date;
+
+public class System_Time {
+    public static void main(String[] args) {
+        Date now = new Date(); // Lấy ra thời gian hiện tại
+
+        System.out.println("Now is: " + now);
+    }
+}
