0418ee7992476a112cf38b73d382000be529d18e
vodinhtuan <vodinhtuan174@gmail.com>
2022-06-13 18:42:53 +0700

    update

---
+++ src/ss3_Arr_vs_Method/Bai_Tap/bt01_index_del.java
@@ -0,0 +1,14 @@
+package ss3_Arr_vs_Method.Bai_Tap;
+
+import java.util.Scanner;
+
+public class bt01_index_del {
+    public static void main(String[] args) {
+        int[] arr = {4, 12, 7, 8, 1, 6, 9};
+
+    }
+    public static int del_index(int[] arr, int x){
+
+        return arr[];
+    }
+}
