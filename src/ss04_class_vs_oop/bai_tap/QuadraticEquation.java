6c39f3fd76c2a157de8a4b144d34e4a59e13d929
vodinhtuan <vodinhtuan174@gmail.com>
2022-06-16 11:02:19 +0700

    update

---
+++ src/ss04_class_vs_oop/bai_tap/QuadraticEquation.java
@@ -0,0 +1,65 @@
+package ss04_class_vs_oop.bai_tap;
+
+public class QuadraticEquation {
+    // Phương trình bặc 2:
+    private double a, b, c;
+
+    public QuadraticEquation() {
+
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA() {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB() {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC() {
+        this.c = c;
+    }
+
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getDiscriminant() {
+        return b * 2 - 4 * a * c;
+    }
+
+    public double getRoot1() {
+        return ((-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / 2 * a);
+    }
+
+    public double getRoot2() {
+        return ((-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / 2 * a);
+    }
+
+    public static void main(String[] args) {
+        QuadraticEquation quadraticEquation = new QuadraticEquation(-8,6,2);
+        System.out.println("Discriminant: " + quadraticEquation.getDiscriminant());
+        if (quadraticEquation.getDiscriminant()>=0){
+            System.out.println("x1= " + quadraticEquation.getRoot1());
+            System.out.println("x2= " + quadraticEquation.getRoot2());
+        }else if (quadraticEquation.getDiscriminant()==0){
+            System.out.println("x1 = x2 = " + -quadraticEquation.getB()/2* quadraticEquation.getA());
+        }else {
+            System.out.println("The equation has no roots");
+        }
+    }
+}
---
+++ src/ss04_class_vs_oop/bai_tap/StopWatch.java
@@ -0,0 +1,48 @@
+package ss04_class_vs_oop.bai_tap;
+
+import java.time.LocalTime;
+
+public class StopWatch {
+    private LocalTime startTime, endTime;
+
+    public StopWatch() {
+        startTime=LocalTime.now();
+        endTime = LocalTime.now();
+    }
+
+    public LocalTime getStartTime() {
+        return getStartTime();
+    }
+
+    /*public void setStartTime() {
+        this.startTime = startTime;
+    }*/
+    public LocalTime getEndTime(){
+        return endTime;
+    }
+    /*public void setEndTime(){
+        this.endTime = endTime;
+    }*/
+    public StopWatch(LocalTime startTime,LocalTime endTime){
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+    public void start(){
+        startTime = LocalTime.now();
+    }
+
+    public void stop(){
+        endTime=LocalTime.now();
+    }
+    public int getElapsedTime(){
+        int miliSecond = ((endTime.getHour()-startTime.getHour())*3600 + (endTime.getMinute()-startTime.getMinute())*60 + (endTime.getSecond()-startTime.getSecond())*1000);
+        return miliSecond;
+    }
+
+    public static void main(String[] args) {
+        StopWatch stopWatch = new StopWatch();
+        System.out.println("Start time: " + stopWatch.startTime);
+        System.out.println("End time: " + stopWatch.endTime);
+        System.out.println("Elapsed time: " + stopWatch.getElapsedTime());
+    }
+}
