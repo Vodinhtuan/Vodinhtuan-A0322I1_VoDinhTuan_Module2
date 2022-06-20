8b754571551d2bd1426943205a48b7bd8a227938
vodinhtuan <vodinhtuan174@gmail.com>
2022-06-20 18:05:54 +0700

    update

---
+++ src/ss06_inheriance/baitap/bt03_point_moveablepoint/MovablePoint.java
@@ -0,0 +1,59 @@
+package ss06_inheriance.baitap.bt03_point_moveablepoint;
+
+public class MovablePoint extends Point {
+    private float xSpeed;
+    private float ySpeed;
+
+    public MovablePoint() {
+    }
+
+    public MovablePoint(float xSpeed, float ySpeed) {
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public MovablePoint(float x, float y, float xSpeed, float ySpeed) {
+        super(x, y);
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    public float getxSpeed() {
+        return xSpeed;
+    }
+
+    public void setxSpeed(float xSpeed) {
+        this.xSpeed = xSpeed;
+    }
+
+    public float getySpeed() {
+        return ySpeed;
+    }
+
+    public void setySpeed(float ySpeed) {
+        this.ySpeed = ySpeed;
+    }
+    public void setSpeed(float xSpeed, float ySpeed){
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+    public float[] getSpeed(){
+        float[] arr = {this.xSpeed, this.ySpeed};
+        return arr;
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint{" +
+                "xSpeed=" + xSpeed +
+                ", ySpeed=" + ySpeed +
+                ", x=" + x +
+                ", y=" + y +
+                '}';
+    }
+    public MovablePoint move(){
+        x +=xSpeed;
+        y +=ySpeed;
+        return this;
+    }
+}
---
+++ src/ss06_inheriance/baitap/bt03_point_moveablepoint/Point.java
@@ -0,0 +1,46 @@
+package ss06_inheriance.baitap.bt03_point_moveablepoint;
+
+public class Point {
+    protected float x;
+    protected float y;
+
+    public Point() {
+    }
+
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+    public void setXY(float x, float y){
+        this.x = x;
+        this.y =y;
+    }
+    public float[] getXY(){
+        float[] arr = {this.x, this.y};
+        return arr;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
